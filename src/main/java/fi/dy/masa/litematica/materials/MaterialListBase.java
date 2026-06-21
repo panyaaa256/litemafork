e3cb5082fc8dade2f5e9c6bdc8df8fb21ccf4f37
panyaaa <panyaaa256@gmail.com>
2026-06-21 10:45:12 +0900

    feat: add toggle material list list type

--- src/main/java/fi/dy/masa/litematica/materials/MaterialListBase.java
+++ src/main/java/fi/dy/masa/litematica/materials/MaterialListBase.java
@@ -6,6 +6,10 @@ import java.util.List;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+
+import fi.dy.masa.malilib.gui.GuiBase;
+import fi.dy.masa.malilib.util.InfoUtils;
+import net.minecraft.util.Mth;
 import com.google.common.collect.ImmutableList;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
@@ -220,9 +224,14 @@ public abstract class MaterialListBase implements IMaterialList
         return this.countMismatched;
     }
 
-    public void setMaterialListType(BlockInfoListType type)
+    public void setMaterialListType(BlockInfoListType type, boolean printMessage)
     {
         this.materialListType = type;
+        if (printMessage) {
+            String val = GuiBase.TXT_GREEN + type.getDisplayName();
+            InfoUtils.printActionbarMessage("malilib.message.set_layer_mode_to", val);
+        }
+
     }
 
     public JsonObject toJson()
@@ -242,7 +251,7 @@ public abstract class MaterialListBase implements IMaterialList
     {
         if (JsonUtils.hasString(obj, "type"))
         {
-            this.setMaterialListType(BlockInfoListType.fromStringStatic(JsonUtils.getString(obj, "type")));
+            this.setMaterialListType(BlockInfoListType.fromStringStatic(JsonUtils.getString(obj, "type")), false);
         }
 
         if (JsonUtils.hasString(obj, "sort_criteria"))
